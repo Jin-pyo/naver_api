dcf6988a2dd123767a915f4d20871fca87faec2b
Jinpyo <jpju@cslee.co.kr>
2022-08-23 21:22:53 +0900

    ICC까지 지도 찍어봄

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,34 +1,10 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <naver-maps :width="600" :height="400" :mapOptions="mapOptions">
+      <naver-marker :lat="36.3770426" :lng=" 127.3930804" @click="onMarkerClicked" @load="onMarkerLoaded"></naver-marker>
+    </naver-maps>
+   
   </div>
 </template>
 
@@ -37,6 +13,27 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      mapOptions: {
+        lat: 36.3770426,
+        lng: 127.3930804,
+        zoom: 17,
+        zoomControl: true,
+        zoomControlOptions: {position: 'TOP_RIGHT'},
+        mapTypeControl: true,
+      }
+    }
+  },
+   methods:{
+    // naver-marker는 다음과 같은 체이닝이 가능합니다.
+    onMarkerLoaded(vue){
+      vue.marker.setDraggable(true).setCursor('').setClickable(true);
+    },
+    onMarkerClicked(event){
+      console.log(event); // 네이버 event 객체
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import naver from 'vue-naver-maps'
+// import vueConfig from 'vue.config'
 
 Vue.config.productionTip = false
+
+Vue.use(naver, {
+  clientID: 'wlbrxdrhiq',
+  useGovAPI: false, //공공 클라우드 API 사용 (선택)
+  subModules:'' // 서브모듈 (선택)
+});
 
 new Vue({
   router,
